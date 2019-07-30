7a9c76911c2bc90446342709f4c66aa8373fd70e
Billy Bennett <bennett.billy@gmail.com>
2019-07-30 15:39:15 -0400

    Updated from C's Github.

--- runway_model.py
+++ runway_model.py
@@ -1,15 +1,13 @@
-## Sample code to generate an image using the
-## pre-trained PGAN celebAHQ-512 checkpoint from the Pytorch Hub
-
-import torch
+import torchvision
 import runway
 import numpy as np
 
+
 @runway.setup(options={"checkpoint": runway.category(description="Pretrained checkpoints to use.",
-                                      choices=['celebAHQ-512', 'celebAHQ-256', 'celeba'],
-                                      default='celebAHQ-512')})
- def setup(opts):
-   checkpoint = opts['checkpoint']
+                                       choices=['celebAHQ-512', 'celebAHQ-256', 'celeba'],
+                                       default='celebAHQ-512')})
+def setup(opts):
+    checkpoint = opts['checkpoint']
     use_gpu = True if torch.cuda.is_available() else False
     # Load the model from the Pytorch Hub
     model = torch.hub.load('facebookresearch/pytorch_GAN_zoo:hub',
@@ -17,11 +15,21 @@ import numpy as np
                            pretrained=True, useGPU=use_gpu)
    return model
 
-use_gpu = True if torch.cuda.is_available() else False
+@runway.command('generate',
+                inputs={ 'z': runway.vector(length=512, sampling_std=0.5)},
+                outputs={ 'image': runway.image })
+   def generate(model, inputs):
+   # Generate ♾ infinite ♾ images
+    z = inputs['z']
+    latents = z.reshape((1, 559))
+    latents = torch.from_numpy(latents)
+    noise, _ = model.buildNoiseData(1)
+    with torch.no_grad():
+        generated_image = model.test(latents.float())
+    generated_image = generated_image.clamp(min=-1, max=1)
+    generated_image = ((generated_image + 1.0) * 255 / 2.0)
+    # Now generated_image contains our generated image! 🌞
+    return generated_image[0].permute(1, 2, 0).numpy().astype(np.uint8)
 
-# Generate one image
-noise, _ = model.buildNoiseData(1)
-with torch.no_grad():
-    generated_image = model.test(noise)
-
-# Now generated_image contains our generated image! 🌞
+ if __name__ == '__main__':
+    runway.run(port=5232)
